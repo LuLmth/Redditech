b9a0cefa3570aa3bb5b569f6dd237e4f51fd57ed
Lucas LAMOTHE <lucas.lamothe@epitech.eu>
2021-10-24 17:46:34 +0200

    [ADD] Get profile image in Header top bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ImageBackground, View, TouchableOpacity } from "react-native";
 import { Searchbar } from "react-native-paper";
 
-import users from "../../fakeDatas/users";
+import { ApiGetRequest } from "../../services/ApiRequest";
+import { getValue } from "../../services/SecureStore";
 
+import users from "../../fakeDatas/users";
 import styles from "./style";
 
 const Header = () => {
+    const [accessToken, setAccessToken] = useState<string | null>(null);
     const [isActive, setIsActive] = useState<boolean>(true);
     const [search, setSearch] = useState<string>("");
+    const [profileImage, setProfileImage] = useState<string>("");
 
     const onChangeText = (text: string) => {
         setSearch(text);
     };
-    const onSubmitEditing = () => {
-        // console.log(search);
+
+    const onSubmitEditing = async () => {
+        // try {
+        //     const searchSubredditData = await ApiGetRequest(`/subreddits/search?q=${search || ''}`, accessToken || "");
+        //     console.log(searchSubredditData);
+        // } catch (e) {
+        //     console.log(e.errors);
+        // }
         // TODO: call API
     };
 
-    // const imageProfile = // Todo: call API
+    useEffect(() => {
+        const fetchToken = async () => {
+            try {
+                const token = await getValue("accessToken");
+                setAccessToken(token);
+            } catch (e) {
+                console.log(e.errors);
+            }
+        };
+        fetchToken();
+    }, []);
+
+    useEffect(() => {
+        const fetchProfileImage = async () => {
+            try {
+                const profileData = await ApiGetRequest(`/api/v1/me?raw_json=1`, accessToken || "");
+                setProfileImage(profileData.subreddit.icon_img);
+            } catch (e) {
+                console.log(e.errors);
+            }
+        };
+        fetchProfileImage();
+    }, [accessToken]);
 
     return (
         <View style={styles.container}>
@@ -28,7 +60,7 @@ const Header = () => {
                 }}
             >
                 <ImageBackground
-                    source={{ uri: users[0].profilePicture }}
+                    source={{ uri: profileImage || users[0].profilePicture }}
                     resizeMode="contain"
                     style={styles.profilePicturePosition}
                     imageStyle={styles.profilePicture}
